bce2978747583e1d4fe06659921375db9e78ee0c
Asim Shankar <ashankar@ashankar-macbookpro.roam.corp.google.com>
2016-01-04 22:09:58 -0800

    android: Simple sanity check before running benchmarks.
    Adding a sanity check to help me debug why using the Android NDK
    toolchain leads to the CGO benchmark being ~3x slower than the pure Go
    implementation on Android, whilst compiling with the
    "arm-linux-gnueabihf" toolchain seems to be ~3x faster.

--- android/main.go
+++ android/main.go
@@ -40,7 +40,30 @@ func BenchmarkPairCGO(b *testing.B) {
 	}
 }
 
-func runBenchmarks(done chan<- struct{}) {
+func sanityTest() bool {
+	var (
+		a, _          = rand.Int(rand.Reader, stdlib.Order)
+		b, _          = rand.Int(rand.Reader, stdlib.Order)
+		stdlibPa      = new(stdlib.G1).ScalarBaseMult(a)
+		stdlibQb      = new(stdlib.G2).ScalarBaseMult(b)
+		stdlibPaired  = stdlib.Pair(stdlibPa, stdlibQb)
+		thislibPa     = new(thislib.G1).ScalarBaseMult(a)
+		thislibQb     = new(thislib.G2).ScalarBaseMult(b)
+		thislibPaired = thislib.Pair(thislibPa, thislibQb)
+	)
+	if got, want := stdlibPaired.String(), thislibPaired.String(); got != want {
+		log.Printf("ERROR: Got %q, want %q", got, want)
+		return false
+	}
+	return true
+}
+
+func runBenchmarks(done chan<- bool) {
+	if !sanityTest() {
+		done <- false
+		return
+	}
+	log.Printf("Pairing sanity check passed")
 	flag.Set("test.benchtime", "2s")
 	benchmarkA, _ = rand.Int(rand.Reader, stdlib.Order)
 	benchmarkB, _ = rand.Int(rand.Reader, stdlib.Order)
@@ -48,19 +71,20 @@ func runBenchmarks(done chan<- struct{}) {
 	log.Printf("BenchmarkPairGo:  %v", std)
 	this := testing.Benchmark(BenchmarkPairCGO)
 	log.Printf("BenchmarkPairCGO: %v", this)
-	close(done)
+	done <- true
 }
 
 func main() {
-	done := make(chan struct{})
+	done := make(chan bool)
 	go runBenchmarks(done)
 	app.Main(func(a app.App) {
 		var glctx gl.Context
+		var success bool
 		ticks := time.Tick(time.Second / 2)
 		black := false
 		for {
 			select {
-			case <-done:
+			case success = <-done:
 				done = nil
 				a.Send(paint.Event{})
 			case <-ticks:
@@ -74,12 +98,17 @@ func main() {
 					if glctx == nil {
 						continue
 					}
-					if done == nil {
+					// solid green:         success
+					// solid red:           failure
+					// flashing black/blue: working
+					if done == nil && success {
 						glctx.ClearColor(0, 1, 0, 1)
+					} else if done == nil && !success {
+						glctx.ClearColor(1, 0, 0, 1)
 					} else if black {
 						glctx.ClearColor(0, 0, 0, 1)
 					} else {
-						glctx.ClearColor(1, 0, 0, 1)
+						glctx.ClearColor(0, 0, 1, 1)
 					}
 					glctx.Clear(gl.COLOR_BUFFER_BIT)
 					a.Publish()
